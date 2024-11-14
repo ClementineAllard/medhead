b12f08e5a81d3c479afae405432008d216e2f934
ClementineAllard <60235896+ClementineAllard@users.noreply.github.com>
2024-11-14 19:32:15 +0100

    Création profil de tests

--- api/src/test/java/com/medhead/api/HopitalControllerTest.java
+++ api/src/test/java/com/medhead/api/HopitalControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -22,7 +23,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.medhead.api.model.Hopital;
 
 
-@SpringBootTest
+@SpringBootTest(classes = ApiApplication.class)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @TestMethodOrder(OrderAnnotation.class) 
 public class HopitalControllerTest {
