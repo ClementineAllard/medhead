59fec8c573efbd3b5292546dce1636d25ce003e9
ClementineAllard <60235896+ClementineAllard@users.noreply.github.com>
2024-11-15 11:30:44 +0100

    Suppression profil

--- api/src/test/java/com/medhead/api/HopitalControllerTest.java
+++ api/src/test/java/com/medhead/api/HopitalControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -23,8 +22,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.medhead.api.model.Hopital;
 
 
-@SpringBootTest(classes = ApiApplication.class)
-@ActiveProfiles("test")
+@SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(OrderAnnotation.class) 
 public class HopitalControllerTest {
