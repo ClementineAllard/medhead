cc5a33697a21c7649d98535be70af20019159af3
ClementineAllard <60235896+ClementineAllard@users.noreply.github.com>
2024-11-18 18:48:43 +0100

    Ecriture tests unitaires webapp

--- webapp/src/test/java/com/medhead/webapp/WebappApplicationTests.java
+++ webapp/src/test/java/com/medhead/webapp/WebappApplicationTests.java
@@ -1,13 +1,77 @@
 package com.medhead.webapp;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WebappApplicationTests {
 
+	@Autowired
+    private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
-	}
+    public void testPageConnexion() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("connexion"))
+            .andExpect(model().attributeExists("utilisateur"));
+    }
+
+	@Test
+    public void testConnexionOK() throws Exception {
+        mockMvc.perform(get("/connexion")
+            .param("email", "admin")
+            .param("mdp", "admin"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("research"))
+            .andExpect(model().attributeExists("groupes"));
+    }
+
+	@Test
+    public void testConnexionKO() throws Exception {
+        mockMvc.perform(get("/connexion")
+            .param("email", "admin")
+            .param("mdp", "adminKO"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("connexion"))
+            .andExpect(model().attribute("messageType", "error"));
+    }
+
+	@Test
+    public void testGetSpecialites() throws Exception {
+        mockMvc.perform(get("/specialites/1"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.msg").value("success"))
+            .andExpect(jsonPath("$.result[0].libelle").value("Anesthésie"));
+    }
+
+	@Test
+    public void testGetHopitauxBySpecialite() throws Exception {
+        mockMvc.perform(get("/hopital/1/Toulouse"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.msg").value("success"))
+            .andExpect(jsonPath("$.result.nom").value("Hôpital Saint-Vincent"));
+    }
+
+	@Test
+    public void testUpDateReservationLit() throws Exception {
+        mockMvc.perform(put("/reservation/2"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.msg").value("success"));
+    }
+
+
 
 }
